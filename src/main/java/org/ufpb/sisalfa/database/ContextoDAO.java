dd1115d062bcf502d4c12995ad8a2eae4f189eb9
robsonalvz <robson.alves@dce.ufpb.br>
2017-10-07 18:50:04 -0300

    first commit

--- src/main/java/org/ufpb/sisalfa/database/ContextoDAO.java
+++ src/main/java/org/ufpb/sisalfa/database/ContextoDAO.java
@@ -3,15 +3,4 @@ package org.ufpb.sisalfa.database;
 import org.ufpb.sisalfa.model.Contexto;
 
 public class ContextoDAO extends GenericDAO <Contexto>{
-	public long retornaIdContexto(){
-		ContextoDAO dao = new ContextoDAO();
-		int ultimo;
-		if (dao.findAll().size()==0){
-			ultimo= 0;
-		}else{
-			ultimo  = dao.findAll().size()-1;
-		}
-		return ultimo; 
-	}
-
 }
